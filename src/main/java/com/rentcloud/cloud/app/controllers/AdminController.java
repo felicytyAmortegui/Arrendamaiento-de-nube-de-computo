16b43791595810b8aba7c8e250fbec2314daa365
felicytyAmortegui <91999614+felicytyAmortegui@users.noreply.github.com>
2021-10-26 14:18:14 -0500

    properties 26 octubre

--- src/main/java/com/rentcloud/cloud/app/controllers/AdminController.java
+++ src/main/java/com/rentcloud/cloud/app/controllers/AdminController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("Admin")
+@CrossOrigin(origins = "*")
 public class AdminController {
     
     @Autowired
--- src/main/java/com/rentcloud/cloud/app/controllers/ClientController.java
+++ src/main/java/com/rentcloud/cloud/app/controllers/ClientController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping ("Client")
+@CrossOrigin(origins = "*")
 public class ClientController {
     
     @Autowired
